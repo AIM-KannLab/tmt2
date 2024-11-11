bd98f3b1c159ef22e8c1be52f5ef759607c543e2
Anna <17569461+zapaishchykova@users.noreply.github.com>
2024-11-11 14:25:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ if __name__ == '__main__':
                         help = 'CSV centiles path, boys model')
     parser.add_argument('--df_centile_girls_csv', '-w',type = str, default = 'percentiles_chart_girls.csv',
                         help = 'CSV centiles path, girls model') 
-    parser.add_argument('--df_centile_boys_csv_csa', '-m',type = str, default = 'percentiles_chart_boys_csa.csv',
+    parser.add_argument('--df_centile_boys_csv_csa', '-m_csa',type = str, default = 'percentiles_chart_boys_csa.csv',
                         help = 'CSV centiles path, boys model csa')
-    parser.add_argument('--df_centile_girls_csv_csa', '-w',type = str, default = 'percentiles_chart_girls_csa.csv',
+    parser.add_argument('--df_centile_girls_csv_csa', '-w_csa',type = str, default = 'percentiles_chart_girls_csa.csv',
                         help = 'CSV centiles path, girls model csa') 
     parser.add_argument('--meta_path', '-pm',type = str, default = 'data/meta.csv',
                         help = 'Path to metafile - only for multiple subjects')
